c8bcf3f54a537b6fe8d051b9023dc269a324e9fb
HiFabioMoreno <fabiomore198@gmail.com>
2026-05-25 08:42:02 -0600

    Init commit

---
+++ Parcial1/Clase2_Calculadora/Calculadora/Calculadora/Form1.Designer.vb
@@ -0,0 +1,318 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        LinkLabel1 = New LinkLabel()
+        Label1 = New Label()
+        mostrar = New TextBox()
+        Digitalizar = New TextBox()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Button15 = New Button()
+        Button16 = New Button()
+        Button17 = New Button()
+        Button18 = New Button()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(51, 261)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(39, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(96, 261)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(39, 23)
+        Button2.TabIndex = 1
+        Button2.Text = "2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(141, 261)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(39, 23)
+        Button3.TabIndex = 2
+        Button3.Text = "3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(51, 232)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(39, 23)
+        Button4.TabIndex = 3
+        Button4.Text = "4"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(96, 232)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(39, 23)
+        Button5.TabIndex = 4
+        Button5.Text = "5"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(141, 232)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(39, 23)
+        Button6.TabIndex = 5
+        Button6.Text = "6"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(51, 203)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(39, 23)
+        Button7.TabIndex = 6
+        Button7.Text = "7"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(96, 203)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(39, 23)
+        Button8.TabIndex = 7
+        Button8.Text = "8"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Location = New Point(141, 203)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(39, 23)
+        Button9.TabIndex = 8
+        Button9.Text = "9"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.Location = New Point(51, 290)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(39, 23)
+        Button10.TabIndex = 9
+        Button10.Text = "0"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button11
+        ' 
+        Button11.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Button11.Location = New Point(51, 174)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(39, 23)
+        Button11.TabIndex = 10
+        Button11.Text = "C"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.AutoSize = True
+        LinkLabel1.Location = New Point(192, 46)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(0, 15)
+        LinkLabel1.TabIndex = 11
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(229, 41)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 15)
+        Label1.TabIndex = 12
+        ' 
+        ' mostrar
+        ' 
+        mostrar.Location = New Point(51, 128)
+        mostrar.Name = "mostrar"
+        mostrar.Size = New Size(174, 23)
+        mostrar.TabIndex = 13
+        ' 
+        ' Digitalizar
+        ' 
+        Digitalizar.Location = New Point(51, 99)
+        Digitalizar.Name = "Digitalizar"
+        Digitalizar.Size = New Size(174, 23)
+        Digitalizar.TabIndex = 14
+        ' 
+        ' Button12
+        ' 
+        Button12.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Button12.Location = New Point(96, 174)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(39, 23)
+        Button12.TabIndex = 15
+        Button12.Text = "<-"
+        Button12.UseVisualStyleBackColor = True
+        ' 
+        ' Button13
+        ' 
+        Button13.Location = New Point(186, 232)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(39, 23)
+        Button13.TabIndex = 16
+        Button13.Text = "+"
+        Button13.UseVisualStyleBackColor = True
+        ' 
+        ' Button14
+        ' 
+        Button14.Location = New Point(186, 203)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(39, 23)
+        Button14.TabIndex = 17
+        Button14.Text = "-"
+        Button14.UseVisualStyleBackColor = True
+        ' 
+        ' Button15
+        ' 
+        Button15.Location = New Point(186, 261)
+        Button15.Name = "Button15"
+        Button15.Size = New Size(39, 23)
+        Button15.TabIndex = 18
+        Button15.Text = "x"
+        Button15.UseVisualStyleBackColor = True
+        ' 
+        ' Button16
+        ' 
+        Button16.Location = New Point(186, 174)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(39, 23)
+        Button16.TabIndex = 19
+        Button16.Text = "/"
+        Button16.UseVisualStyleBackColor = True
+        ' 
+        ' Button17
+        ' 
+        Button17.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Button17.Location = New Point(141, 174)
+        Button17.Name = "Button17"
+        Button17.Size = New Size(39, 23)
+        Button17.TabIndex = 20
+        Button17.Text = "( )"
+        Button17.UseVisualStyleBackColor = True
+        ' 
+        ' Button18
+        ' 
+        Button18.Font = New Font("Segoe UI", 9F)
+        Button18.ForeColor = SystemColors.MenuHighlight
+        Button18.Location = New Point(96, 290)
+        Button18.Name = "Button18"
+        Button18.Size = New Size(129, 23)
+        Button18.TabIndex = 21
+        Button18.Text = "="
+        Button18.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 14F, FontStyle.Bold)
+        Label2.ForeColor = SystemColors.ButtonFace
+        Label2.Location = New Point(84, 38)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(117, 25)
+        Label2.TabIndex = 22
+        Label2.Text = "Calculadora"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.HotTrack
+        ClientSize = New Size(280, 355)
+        Controls.Add(Label2)
+        Controls.Add(Button18)
+        Controls.Add(Button17)
+        Controls.Add(Button16)
+        Controls.Add(Button15)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Button12)
+        Controls.Add(Digitalizar)
+        Controls.Add(mostrar)
+        Controls.Add(Label1)
+        Controls.Add(LinkLabel1)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        ForeColor = SystemColors.HotTrack
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents mostrar As TextBox
+    Friend WithEvents Digitalizar As TextBox
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ Parcial1/Clase2_Calculadora/Calculadora/Calculadora/Form1.vb
@@ -0,0 +1,176 @@
+﻿Imports System.Reflection.Emit
+
+Public Class Form1
+
+    Dim operacion As String = ""
+    Dim parentesisAbierto As Boolean = False
+
+    Private Sub AgregarTexto(valor As String)
+
+        operacion &= valor
+        Digitalizar.Text = operacion
+
+        Actualizar()
+
+    End Sub
+
+    Private Sub Actualizar()
+
+        Try
+
+            Dim dt As New DataTable()
+            Dim resultado = dt.Compute(operacion, Nothing)
+
+            If resultado.ToString.Contains("+") Or
+                 resultado.ToString.Contains("-") Or
+                 resultado.ToString.Contains("*") Or
+                 resultado.ToString.Contains("/") Then
+
+                'operacion = resultado.ToString()
+                'Label3.Text = operacion
+                mostrar.Text = resultado.ToString()
+
+            End If
+
+        Catch ex As Exception
+
+            mostrar.Text = ""
+
+        End Try
+
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        AgregarTexto("1")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        AgregarTexto("2")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        AgregarTexto("3")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        AgregarTexto("4")
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        AgregarTexto("5")
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        AgregarTexto("6")
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        AgregarTexto("7")
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        AgregarTexto("8")
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        AgregarTexto("9")
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        AgregarTexto("0")
+    End Sub
+
+    'borrar toda la operacion y el resultado
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+
+        operacion = ""
+        Digitalizar.Text = ""
+        mostrar.Text = "0"
+        parentesisAbierto = False
+
+    End Sub
+
+    'borrar un digito
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+
+        If operacion.Length > 0 Then
+
+            operacion = operacion.Substring(0, operacion.Length - 1)
+
+            Digitalizar.Text = operacion
+
+            If operacion = "" Then
+                mostrar.Text = "0"
+            Else
+                Actualizar()
+            End If
+
+        End If
+
+    End Sub
+
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        AgregarTexto("+")
+    End Sub
+
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        AgregarTexto("-")
+    End Sub
+
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        AgregarTexto("*")
+    End Sub
+
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        AgregarTexto("/")
+    End Sub
+
+
+    ' Agregar parentesis o cerrar el parentesis
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+
+        If parentesisAbierto = False Then
+
+            AgregarTexto("(")
+            parentesisAbierto = True
+
+        Else
+
+            AgregarTexto(")")
+            parentesisAbierto = False
+
+        End If
+
+    End Sub
+
+    ' Obtener el resultado de la operacion
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+
+        Try
+
+            Dim dt As New DataTable()
+            Dim resultado = dt.Compute(operacion, Nothing)
+
+            mostrar.Text = resultado.ToString()
+
+
+        Catch ex As Exception
+
+            mostrar.Text = "Error"
+
+        End Try
+
+
+
+    End Sub
+
+
+End Class
